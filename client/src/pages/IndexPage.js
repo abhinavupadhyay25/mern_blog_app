fd2c95876cc9f6bf592a893b2554ce80387598e9
Abhinav Upadhyay <abhinavupadhyay731@gmail.com>
2023-07-15 11:24:57 +0530

    first commit

---
+++ client/src/pages/IndexPage.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import Post from "../Post";
+
+export default function IndexPage() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("/post").then((response) => {
+      response.json().then((posts) => {
+        setPosts(posts);
+      });
+    });
+  }, []);
+
+  return <>{posts.length > 0 && posts.map((post) => <Post {...post} />)}</>;
+}
